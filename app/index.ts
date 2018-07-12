f25720b716c17624f7fbda1bd9c91b8fa984bdbc
saiumesh535 <d.sai535@gmail.com>
2018-07-12 18:51:28 +0530

    added ts config

---
+++ app/index.ts
@@ -0,0 +1,19 @@
+import { checkForMissingData, validate as v } from './validation';
+
+
+export function validate (input, config) {
+  
+  if (input === undefined || typeof input !== 'object') {
+    throw new Error('Input should be object, also not undefined');
+  }
+
+  if (config === undefined || typeof config !== 'object') {
+    throw new Error('config should be object, also not undefined');
+  }
+
+  const missingData = checkForMissingData(input, config);
+  const errors = missingData.length === 0 && v(input, config);
+
+  return !!errors ? [...missingData, ...errors] : missingData;
+
+}
--- validation.js
+++ validation.js
@@ -1,54 +1,56 @@
-export function checkForMissingData(input, config) {
-  const inputKeys = Object.keys(input);
-  const configKeys = Object.keys(config);
-  const errors = configKeys.reduce((cumm, data) => {
-    if (inputKeys.indexOf(data) === -1) {
-      return [...cumm, { for: data, errorType: 'data missing' }];
-    }
-    return cumm;
-  }, []);
-  return errors;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function checkForMissingData(input, config) {
+    var inputKeys = Object.keys(input);
+    var configKeys = Object.keys(config);
+    var errors = configKeys.reduce(function (cumm, data) {
+        if (inputKeys.indexOf(data) === -1) {
+            return cumm.concat([{ for: data, errorType: 'data missing' }]);
+        }
+        return cumm;
+    }, []);
+    return errors;
 }
-export function validate(input, config) {
-  /* get keys from input and pass key, input and config */
-  const errors = Object.keys(input).reduce((cumm, value) => {
-    if (Boolean(config[value])) {
-      return [...cumm, ...doValidation(value, input, config)];
-    }
-    else {
-      return cumm;
-    }
-  }, []);
-  return errors;
+exports.checkForMissingData = checkForMissingData;
+function validate(input, config) {
+    /* get keys from input and pass key, input and config */
+    var errors = Object.keys(input).reduce(function (cumm, value) {
+        if (Boolean(config[value])) {
+            return cumm.concat(doValidation(value, input, config));
+        }
+        else {
+            return cumm;
+        }
+    }, []);
+    return errors;
 }
-
+exports.validate = validate;
 function doValidation(key, input, config) {
-  const validation = Object.keys(config[key]).reduce((cumm, value) => {
-    const isValid = validator[value] && validator[value](input[key], config[key][value]);
-    if (!isValid) {
-      const errorType = value === 'type' ? config[key][value] : value
-      return [...cumm, { for: key, errorType, value: config[key][value] }]
-    } else {
-      return isValid === 1 ? [...cumm, { for: key, message: `missing type config for ${key}` }] : cumm;
-    }
-  }, []);
-  return validation;
+    var validation = Object.keys(config[key]).reduce(function (cumm, value) {
+        var isValid = validator[value] && validator[value](input[key], config[key][value]);
+        if (!isValid) {
+            var errorType = value === 'type' ? config[key][value] : value;
+            return cumm.concat([{ for: key, errorType: errorType, value: config[key][value] }]);
+        }
+        else {
+            return isValid === 1 ? cumm.concat([{ for: key, message: "missing type config for " + key }]) : cumm;
+        }
+    }, []);
+    return validation;
 }
-
-const validator = {
-  min: (input, value) => input.length >= value,
-  max: (input, value) => input.length <= value,
-  type: (input, value) => {
-    if (!typedValidations[value]) {
-      return 1;
-    }
-    return typedValidations[value](input)
-  },
+var validator = {
+    min: function (input, value) { return input.length >= value; },
+    max: function (input, value) { return input.length <= value; },
+    type: function (input, value) {
+        if (!typedValidations[value]) {
+            return 1;
+        }
+        return typedValidations[value](input);
+    },
 };
-
 /* typed validations */
-const typedValidations = {
-  email: (input) => /^([\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+\.)*[\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+@((((([a-z0-9]{1}[a-z0-9\-]{0,62}[a-z0-9]{1})|[a-z])\.)+[a-z]{2,6})|(\d{1,3}\.){3}\d{1,3}(\:\d{1,5})?)$/i.test(input), // example validation you can write your own regex
-  number: (input) => !isNaN(input),
-  boolean: (input) => input.toLowerCase() === ('true' || 'false') ? true : false,
-}
+var typedValidations = {
+    email: function (input) { return /^([\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+\.)*[\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+@((((([a-z0-9]{1}[a-z0-9\-]{0,62}[a-z0-9]{1})|[a-z])\.)+[a-z]{2,6})|(\d{1,3}\.){3}\d{1,3}(\:\d{1,5})?)$/i.test(input); },
+    number: function (input) { return !isNaN(input); },
+    boolean: function (input) { return input.toLowerCase() === ('true' || 'false') ? true : false; },
+};
