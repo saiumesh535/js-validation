41d9b4971c5b980a0cc8e69998dfda819bc90880
= <=>
2018-07-16 18:11:24 +0530

    fixed versions of package

--- spec/validation/validation.spec.js
+++ spec/validation/validation.spec.js
@@ -1,47 +1,47 @@
-var validation=require("../../index");
+var validation = require("../../index");
 
 
-describe("Validation",function(){
+describe("Validation", function () {
 
     console.log("Executing Tests!!");
 
-    it("should verify missing data",function() {
+    it("should verify missing data", function () {
         const config = {
             name: {
-            max: 22,
-            min: 3,
+                max: 22,
+                min: 3,
             }
         }
-        
+
         const input = {};
         const isValid = validation.validate(input, config);
         expect(isValid[0].errorType).toContain('data missing');
     });
 
-    it("should verify minimum and maximum length",function() {
-    const config = {
-        name: {
-        max: 22,
-        min: 3,
-        }
-    }
-    
-    const input = {
-        name: 'sai umesh'
-    }
-    const isValid = validation.validate(input, config);
-    expect(isValid.length).toEqual(0);
-    });
-
-    it("should verify if the email is invalid",function() {
+    it("should verify minimum and maximum length", function () {
         const config = {
-            email: {
-            max: 22,
-            min: 3,
-            type:'email'
+            name: {
+                max: 22,
+                min: 3,
             }
         }
-        
+
+        const input = {
+            name: 'sai umesh'
+        }
+        const isValid = validation.validate(input, config);
+        expect(isValid.length).toEqual(0);
+    });
+
+    it("should verify if the email is invalid", function () {
+        const config = {
+            email: {
+                max: 22,
+                min: 3,
+                type: 'email'
+            }
+        }
+
         const input = {
             email: 'sai.umesh@gmail@.com'
         }
